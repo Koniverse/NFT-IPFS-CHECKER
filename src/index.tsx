35f27389893a00edaf3e4b59009678b8a05fdb82
Thiendekaco <thienguo@gmail.com>
2023-08-22 02:26:27 +0700

    'issues 1,2,3'

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GateWayProvider from "./provider/GateWay.Provider";
+import ThemeProviderStyled from "./provider/Theme.Provider";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <ThemeProviderStyled >
+          <GateWayProvider>
+                <App />
+          </GateWayProvider>
+      </ThemeProviderStyled>
   </React.StrictMode>
 );
 
