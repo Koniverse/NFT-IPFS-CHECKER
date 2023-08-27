61711ce7f0a4307f5618e062153a10b36d983078
Thiendekaco <thienguo@gmail.com>
2023-08-27 20:38:11 +0700

    Updates

--- src/component/spinner/Spinner.component.tsx
+++
@@ -1,9 +0,0 @@
-import { SpinnerContainer, SpinnerOverLay } from "./Spinner.styles";
-
-const Spinner = () => (
-    <SpinnerOverLay>
-        <SpinnerContainer />
-    </SpinnerOverLay>
-);
-
-export default Spinner;
--- src/context/index.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-import { gateWayInterface } from "../types";
-import gateWay from "../utils/gateWay";
-import {isNumberObject} from "util/types";
-export const GatewayContext = createContext<gateWayInterface>({
-    gateway : gateWay,
-    setGateway : () => {}
-})
